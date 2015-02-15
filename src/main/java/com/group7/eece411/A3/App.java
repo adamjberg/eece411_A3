fb1ce9f60db4f3f6406aaed31b511a2be19e717b
Isha <isha.kumaarr@gmail.com>
2015-02-15 14:10:14 -0800

    Add outline of program execution to main

--- src/main/java/com/group7/eece411/A3/App.java
+++ src/main/java/com/group7/eece411/A3/App.java
@@ -1,13 +1,37 @@
 package com.group7.eece411.A3;
 
+import java.util.Vector;
+
+import com.group7.eece411.A3.UDPClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	private static Vector<Header> receivedUniqueHeaders = new Vector<Header>();
+	
+    public static void main( String[] args ) throws Exception {
+    	UDPClient client = new UDPClient(7777);
+		client.setTimeout(0);
+		
+		do {
+			// Receive bytes and init empty byte array for the actual message
+			byte[] receivedBytes = client.receive();
+			byte[] message;
+			
+			// Create new Header obj for this received message
+			Header header = new Header();
+			message = header.decodeAndGetMessage(receivedBytes);
+			
+			// Check if header already in our vector of previously received headers
+			if (receivedUniqueHeaders.contains(header)) {
+				// TODO: do something with the dupe request
+			} else {
+				receivedUniqueHeaders.add(header);
+				// TODO: do something with this new request
+			}
+			
+		} while(true);
     }
 }
