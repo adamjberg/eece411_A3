bcd33a8adb1492bc7d838d6a25b8fe1cef6dd3e0
Ehsan70 <a.ehsan70@gmail.com>
2015-02-15 21:01:59 -0800

    partially did the RequestData/ResponseData. Need to fix val_len and add comments

--- src/main/java/com/group7/eece411/A3/RequestData.java
+++ src/main/java/com/group7/eece411/A3/RequestData.java
@@ -1,16 +1,21 @@
 /**
  * 
  */
-package com.group7.eece411.A3;
+package eece411_assg3_b;
 
-import javax.swing.Spring;
+import java.util.Arrays;
+import java.util.HashMap;
+
 
 /**
  * @author Ehsan
  * 
  */
-public class RequestData {
+public class RequestData extends Protocol {
 
+	HashMap <String, byte[]>  HMdata = new HashMap<String, byte[]> (); 
+
+	public static final int MAX_REQUEST_SIZE = 15005; // 15000 + 2 + 32 + 1 bytes 
 	public static final int KEY_SIZE = 32;
 	public static final int VALUE_SIZE = 15000;
 
@@ -21,53 +26,124 @@ public class RequestData {
 
 	public RequestData(byte c, byte[] k, int v_l, byte[] r_v) {
 		// constructor stub
-		command = c;
-		key = k;
-		val_len = v_l;
-		value = r_v;
+		byte[] c_arr = new byte[1];
+		c_arr[1] = c;
+		HMdata.put("command", c_arr);
+		HMdata.put("key", Conversions.int2leb(v_l, 0));
+		HMdata.put("val_len", Conversions.int2leb(v_l, 0));
+		HMdata.put("value", r_v);
 	}
 
-	public String commandMaker() throws IllegalArgumentException {
+	public String requestMaker() throws IllegalArgumentException {
 		StringBuilder sb = new StringBuilder();
 		if (value.length > VALUE_SIZE || val_len > VALUE_SIZE) {
 			throw new IllegalArgumentException();
 		} else {
-			sb.append(Conversions.byteToHexString(command));
-			sb.append(Conversions.byteArrayToHexString(key));
-			sb.append(String.valueOf(val_len));
-			sb.append(Conversions.byteArrayToHexString(value));
+			sb.append(Conversions.byteArrayToHexString(HMdata.get("command")));
+			sb.append(Conversions.byteArrayToHexString(HMdata.get("key")));
+			sb.append(String.valueOf(HMdata.get("val_len")));
+			sb.append(Conversions.byteArrayToHexString(HMdata.get("value")));
 		}
 		return sb.toString();
-	}
-
-	public void  commandDestrigyfy() {
-		// todo
 	}
 
 	/*
 	 * 
 	 * */
-	public void put( byte [] k, byte [] res )
+	public void put( byte [] k, byte [] val )
 	{
-		command = 0x01 ; 
-		key = k ; 
-		value = res;
-		val_len = res.length;
+		byte[] c_arr = { 0x01 };
+		HMdata.put("command", c_arr);
+		HMdata.put("key", k);
+		HMdata.put("val_len", Conversions.int2leb(val.length, 0));
+		HMdata.put("value", val);
 	}
 
 	/*
+	 * Remember you are constructing the object you are not actually sending the put command. 
 	 * Val is a HEX String
 	 * */	
-	public void put( String k, String val )
+	public void ConstructPut( String k, String val )
 	{
-		command = 0x01 ; 
-		key = Conversions.hexStringToByteArray(k) ; 
-		value = Conversions.hexStringToByteArray( val);
-		val_len = value.length;
+		byte [] val_arr = Conversions.hexStringToByteArray(val);
+		byte[] c_arr = { 0x01 };
+		HMdata.put("command", c_arr);
+		HMdata.put("key", Conversions.hexStringToByteArray(k));
+		HMdata.put("val_len", Conversions.int2leb(val_arr.length, 0));
+		HMdata.put("value", val_arr);
+	}	
+	
+	
+	public void ConstructGet( byte [] key )
+	{
+		byte[] c_arr = { 0x02 };
+		HMdata.put("command", c_arr);
+		HMdata.put("key", key);
+		HMdata.put("val_len", Conversions.int2leb(-1, 0));
+	}	
+	
+	
+	/*
+	 * Prolly need to change the argument to int type.  
+	 * */
+	public void ConstructRemove( byte [] k)
+	{
+		byte[] c_arr = { 0x03 };
+		HMdata.put("command", c_arr);
+		HMdata.put("key", key);
+		HMdata.put("val_len", Conversions.int2leb(-1, 0));
 	}
-	public void get( byte [] k )
+
+	@Override
+	public boolean isValidate() {
+		//Todo
+		
+		return false;
+	}
+
+	@Override
+	public Protocol convert(byte[] d) {
+		if (d.length > MAX_REQUEST_SIZE)
+		{
+			throw new IllegalArgumentException();
+		}
+		//Create a new instance to better preserve data.
+		ResponseData rd  = new ResponseData();
+		
+		// Retrieve the data from the array
+		byte command_code = d[0]; // The first elemt is the Response code 		
+		
+		byte [] key= Arrays.copyOfRange(d, 1, 33) ; // the [1:33) the elements are the length
+		byte [] val_len = Arrays.copyOfRange(d, 33, 37) ; // the [33:37) the elements are the length
+		int val_len_int = Conversions.leb2int(val_len, 0);		
+		byte [] val = Arrays.copyOfRange(d, 37, d.length);
+		
+		//check the validity of the retrieved data 
+		if (!checkCommandCode(command_code))
+		{
+			throw new IllegalArgumentException(); 
+		}
+		
+		//Check the length of val
+		if (val_len_int > VALUE_SIZE || val_len_int<0 || val.length >VALUE_SIZE )
+		{
+			throw new IllegalArgumentException(); 
+		}
+		return rd;
+	}
+	
+	boolean checkCommandCode(byte comm)
 	{
-		command = 0x02 ; 
-		key = k ; 
+		if ( comm == 0x01 || comm == 0x02 || comm == 0x03 || comm == 0x04)
+		{
+			return true; 
+		}
+		else 
+			return true ;
+	}
+
+	@Override
+	public byte[] getHeader(String Head) {
+		return HMdata.get(Head);
 	}
 }
--- src/main/java/com/group7/eece411/A3/ResponseData.java
+++ src/main/java/com/group7/eece411/A3/ResponseData.java
@@ -1,17 +1,137 @@
-package com.group7.eece411.A3;
+package eece411_assg3_b;
 
-public class ResponseData {
-	public byte response_code;
-	public int val_len;
-	public byte [] response_value;
-	
-	
-	
-	public ResponseData(byte r_c,int v_l, byte [] r_v) {
-		// constructor stub
-		response_code=r_c;
-		val_len = v_l;
-		response_value = r_v;
-			
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ResponseData extends Protocol {
+
+	HashMap<String, byte[]> HMdata = new HashMap<String, byte[]>();
+	public static final int MAX_RESPONSE_SIZE = 15005; // 15000 + 1 + 4 bytes 
+	public static final int KEY_SIZE = 32;
+	public static final int VALUE_SIZE = 15000;
+
+	@SuppressWarnings("unchecked")
+	public ResponseData(byte r_c, int v_l, byte[] r_v) {
+		byte[] rc = new byte[1];
+		rc[1] = r_c;
+		HMdata.put("response_code", rc);
+		HMdata.put("val_len", Conversions.int2leb(v_l, 0));
+		HMdata.put("response_value", r_v);
 	}
+
+	public ResponseData() {
+	}
+
+	public String responseMaker(byte response_code, int val_len, byte[] value_in)
+			throws IllegalArgumentException {
+		StringBuilder sb = new StringBuilder();
+		if (value_in.length > VALUE_SIZE || val_len > VALUE_SIZE) {
+			throw new IllegalArgumentException();
+		} else {
+			sb.append(Conversions.byteArrayToHexString((HMdata
+					.get("response_code"))));
+			// Converts the int to little-endian array and then stringifies it
+			sb.append(Conversions.byteArrayToHexString(Conversions.int2leb(
+					val_len, 0)));
+			sb.append(Conversions.byteArrayToHexString(value_in));
+		}
+		return sb.toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	public void ConstructGetSuccess(byte[] value) {
+		byte[] b = { 0x00 };
+		HMdata.put("response_code", b);
+		HMdata.put("response_value", value);
+		HMdata.put("val_len", Conversions.int2leb(value.length, 0));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void ConstructPutSuccess() {
+		byte[] b = { 0x00 };
+		HMdata.put("response_code", b);
+		HMdata.put("val_len", Conversions.int2leb(-1, 0));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void ConstructNonExistanceKey() {
+		byte[] b = { 0x01 };
+		HMdata.put("response_code", b);
+		HMdata.put("val_len", Conversions.int2leb(-1, 0));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void ConstructOutOfSpace() {
+		byte[] b = { 0x02 };
+		HMdata.put("response_code", b);
+		HMdata.put("val_len", Conversions.int2leb(-1, 0));
+	}
+
+	@Override
+	public byte[] getHeader(String Head) {
+		return HMdata.get(Head);
+	}
+
+	/* Not sure if are ever gonna use it but ... */
+	public String strigyfy() throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+		outputStream.write(HMdata.get("response_code"));
+		outputStream.write(HMdata.get("val_len"));
+		outputStream.write(HMdata.get("response_value"));
+
+		return Conversions.byteArrayToHexString(outputStream.toByteArray());
+	}
+
+	@Override
+	public boolean isValidate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@SuppressWarnings("static-access")
+	@Override
+	public Protocol convert(byte[] d)  {
+		if (d.length > MAX_RESPONSE_SIZE)
+		{
+			throw new IllegalArgumentException();
+		}
+		//Create a new instance to better preserve data.
+		ResponseData rd  = new ResponseData();
+		
+		// Retrieve the data from the array
+		byte respon_code = d[0]; // The first elemt is the Response code 		
+		byte [] val_len = Arrays.copyOfRange(d, 1, 5) ; // the 1,2,3,4 the elements are the length
+		int val_len_int = Conversions.leb2int(val_len, 0);
+		byte [] val = Arrays.copyOfRange(d, 5, d.length);
+		
+		//check the validity of the retrieved data 
+		if (!checkResponseCode(respon_code))
+		{
+			throw new IllegalArgumentException(); 
+		}
+		
+		//Check the length of val
+		if (val_len_int > VALUE_SIZE || val_len_int<0 || val.length >VALUE_SIZE )
+		{
+			throw new IllegalArgumentException(); 
+		}
+		return rd;
+	}
+	
+	
+	private static boolean checkResponseCode (byte code)
+	{
+		if (code == 0x00 || code == 0x01 || code == 0x02 || code == 0x03 || code == 0x04 || code == 0x05)
+		{
+			return true; 
+		}
+		else 
+			return true ;
+		
+	}
+	
+
 }
